892f4b41db3c9833db2633b5b790582f8f695d09
Abhishek_yadav <abhi991926@gmail.com>
2022-07-17 01:25:34 -0700

    using round average of numbers

--- average_of_three_numbers.py
+++ average_of_three_numbers.py
@@ -2,4 +2,4 @@ num1=int(input("enter the first numbers "))
 num2=int(input("enter the second numbers "))
 num3=int(input("enter the third numbers "))
 average_of_numbers=((num1+num2+num3))/3
-print(average_of_numbers)
+print(round(average_of_numbers,2))
